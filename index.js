396ecc61433073be2b9a914e9021755cae5cf7cd
Hafiz Maaz Anis <89415688+Maaz48@users.noreply.github.com>
2023-02-11 06:37:59 +0500

    image upload api with image url res

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const multer = require("multer");
-const mongoose = require("mongoose");
 
 const app = express();
 
@@ -19,47 +18,15 @@ app.use("/uploads", express.static("uploads"));
 
 const upload = multer({ storage });
 
-mongoose
-  .connect(
-    "mongodb+srv://huzaifa:fireburning300@cluster0.xxwlzho.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log("connected....");
-  });
-
-const ImageSchema = new mongoose.Schema({
-  image: {
-    type: String,
-    required: true,
-  },
-});
-
-const Image = mongoose.model("Image", ImageSchema);
-
 app.use(bodyParser.json());
 
 app.post("/image", upload.single("image"), (req, res) => {
-  const newImage = new Image({
-    image: req.file.path,
-  });
-  console.log(req.file.path);
-
-  newImage.save((error) => {
-    if (error) {
-      return res.status(400).json({
-        success: false,
-        message: "Could not save image to database.",
-      });
-    }
-
-    return res.status(200).json({
-      success: true,
-      message: "Image was saved to the database successfully.",
-    });
+  return res.status(200).json({
+    success: true,
+    message: "Image was saved to the database successfully.",
+    image: `http://localhost:3000/uploads/${req.file.filename}`,
   });
 });
-
-app.get("/vides", (req, res) => {});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
